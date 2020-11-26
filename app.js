0db583a25c87c00182886e78a36a3cc2e7db7823
Irvin Lin <irvinln@gmail.com>
2020-11-26 22:57:28 +0800

    Add Multer & Respective Controllers for file uploads & Linking Routes to Controllers

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var uploadRouter = require('./routes/upload');
+var filesRouter = require('./routes/files');
+
 var app = express();
 
 // view engine setup
@@ -21,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/files', filesRouter);
+app.use('/upload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/upload.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+const controller = require("../controller/file.controller");
+
+router.post('/', controller.upload);
+
+module.exports = router;
